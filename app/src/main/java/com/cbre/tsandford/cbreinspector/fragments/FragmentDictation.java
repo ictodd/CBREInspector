4e71a1d623217b49d2da0534703c28bb7a42914a
Todd Sandford <todd.sandford@cbre.co.nz>
2018-10-23 12:06:20 +1300

    Tested the functionality of Recorder. Works as expected. Created custom PlayButton. Need to add functionality - add recording to gallery, play recording toggle.

--- app/src/main/java/com/cbre/tsandford/cbreinspector/fragments/FragmentDictation.java
+++ app/src/main/java/com/cbre/tsandford/cbreinspector/fragments/FragmentDictation.java
@@ -1,37 +1,49 @@
 package com.cbre.tsandford.cbreinspector.fragments;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageButton;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.cbre.tsandford.cbreinspector.AppState;
 import com.cbre.tsandford.cbreinspector.R;
 
 import java.io.IOException;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class FragmentDictation extends Fragment {
 
-    private ImageButton record_btn;
-    boolean recording = false;
+    // region Private Members
 
+    private ImageButton record_btn;
+    private boolean recording = false;
     private MediaRecorder recorder = null;
     private MediaPlayer player = null;
-
     private String active_clip = null;
+    private LinearLayout gallery_view_parent = null;
+
+    // endregion
+
+    private ImageButton test_btn;
 
     public FragmentDictation() {
         // Required empty public constructor
     }
+
+    // region Fragment Events
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,43 +62,27 @@ public class FragmentDictation extends Fragment {
                 toggle_button();
             }
         });
+        gallery_view_parent = getActivity().findViewById(R.id.voice_note_gallery);
+        test_btn = getActivity().findViewById(R.id.btn_test_voice_playback);
     }
 
-    private void toggle_button() {
-        if(!recording){
-            record_btn.setImageResource(R.drawable.microphone_disabled);
-            stopRecording();
-        }else{
-            record_btn.setImageResource(R.drawable.microphone_recording);
-            startRecording();
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(recorder != null){
+            recorder.release();
+            recorder = null;
+        }
+
+        if(player != null){
+            player.release();
+            player = null;
         }
     }
 
-    private void onPlay(boolean start){
-        if(start){
-            startPlaying();
-        }else{
-            stopPlaying();
-        }
-    }
+    //endregion
 
-    private void startPlaying(){
-        player = new MediaPlayer();
-        try{
-            player.setDataSource(active_clip);
-            player.prepare();
-            player.start();
-        }catch(IOException ex){
-            Log.d("TODD", "prepare() failed");
-        }
-    }
-
-    private void stopPlaying(){
-        player.release();
-        player = null;
-    }
-
-    // todo make sure this recorder actually works...
+    // region Recording Methods
 
     private void startRecording(){
         active_clip = AppState.ActiveInspection.audio_clips.get_new_resource().getPath();
@@ -107,22 +103,102 @@ public class FragmentDictation extends Fragment {
     }
 
     private void stopRecording(){
-        recorder.stop();
-        recorder.release();
-        recorder = null;
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
         if(recorder != null){
+            recorder.stop();
             recorder.release();
             recorder = null;
         }
+    }
 
-        if(player != null){
-            player.release();
-            player = null;
+    // endregion
+
+    // region Playing Methods
+
+    private void startPlaying(){
+        player = new MediaPlayer();
+        try{
+            player.setDataSource(active_clip);
+            player.prepare();
+            player.start();
+        }catch(IOException ex){
+            Log.d("TODD", "prepare() failed");
         }
     }
+
+    private void stopPlaying(){
+        player.release();
+        player = null;
+    }
+
+    // endregion
+
+    private void onPlay(boolean start){
+        if(start){
+            startPlaying();
+        }else{
+            stopPlaying();
+        }
+    }
+
+    private void toggle_button() {
+        if(recording){
+            record_btn.setImageResource(R.drawable.microphone_disabled);
+            stopRecording();
+        }else{
+            record_btn.setImageResource(R.drawable.microphone_recording);
+            startRecording();
+        }
+        recording = !recording;
+    }
+
+    // region Gallery Methods
+
+    private void clear_gallery(){
+        this.gallery_view_parent.removeAllViews();
+    }
+
+    private void reload_voice_notes(){
+        clear_gallery();
+        List<Uri> voice_notes = AppState.ActiveInspection.audio_clips.get_all_items(10);
+        for(Uri voice_note : voice_notes){
+            if(!gallery_contains_voice_note(voice_note.getPath()))
+                add_voice_note_to_gallery(voice_note);
+        }
+    }
+
+    private boolean gallery_contains_voice_note(String path){
+        int number_of_children = gallery_view_parent.getChildCount();
+        for(int i = 0; i < number_of_children; i ++){
+            if(gallery_view_parent.getChildAt(i).getTag().toString() == path)
+                return true;
+        }
+        return false;
+    }
+
+    private void add_voice_note_to_gallery(Uri voice_note_uri){
+//
+//        LinearLayout.LayoutParams view_params = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.WRAP_CONTENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT);
+//        view_params.bottomMargin = 5;
+//        view_params.leftMargin = 5;
+//        view_params.rightMargin = 5;
+//        view_params.topMargin = 5;
+//
+//        ImageView new_img_view = new ImageView(this.getActivity());
+//        new_img_view.setImageURI(voice_note_uri);
+//        new_img_view.setAdjustViewBounds(true);
+//        new_img_view.setTag(voice_note_uri.getPath());
+//
+//        this.registerForContextMenu(new_img_view);
+//
+//        Drawable background = getActivity().getDrawable(R.drawable.full_border_thin);
+//        new_img_view.setBackground(background);
+//
+//        this.gallery_view.addView(new_img_view, view_params);
+    }
+
+    // endregion
+
 }
+
